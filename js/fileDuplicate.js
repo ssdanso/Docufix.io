b42095a98d4ce4ca315aad380381b6c96905ad18
Onasanya Tunde <Onasanyatunde67@gmail.com>
2019-11-02 16:07:33 +0100

    Made th file duplicate mobile responsive

--- js/fileDuplicate.js
+++ js/fileDuplicate.js
@@ -25,15 +25,18 @@ function showFile() {
 document.getElementById("file").style.display = "block";
 document.getElementById("text").style.display = "none";
 document.getElementById("fileBtn").classList.add("active1");
-document.getElementById("textBtn").classList.remove("tab-active");
-document.getElementById('fileBtn').classList.add('tab-active');
+document.getElementById("textBtn").classList.remove("active1");
+
+
+// document.getElementById("textBtn").classList.remove("active");
+// document.getElementById('fileBtn').classList.add('tab-active');
 document.getElementById('resultArea').style.display = "none";
 }
 function showtext() {
 document.getElementById("text").style.display = "block";
 document.getElementById("file").style.display = "none";
-document.getElementById("textBtn").classList.add("tab-active");
-document.getElementById('fileBtn').classList.remove('tab-active')
+document.getElementById("textBtn").classList.add("active1");
+document.getElementById('fileBtn').classList.remove('active1')
 
 }
 
@@ -136,17 +139,17 @@ function getCookie(name) {
                 },
                 success: function(response){
                     console.log('success');
-                    console.log(response);
+                    console.log(response.responseJSON);
                     $(".progress").attr("style", "display: none " );
                     progress.remove()
                     $('#textareaDifferences').val(response.file1)
                     toastr.success("Content Loaded Successfully");
                     
                 },
-                error: function(){
+                error: function(response){
                     $(".progress").attr("style", "display: none " );
                     toastr.error("An Error Occured");
-                    console.log("Error Occured");
+                    console.log(response.responseJSON);
                     
                 },
                 cache: false,
@@ -209,3 +212,90 @@ function getCookie(name) {
 })
 
 
+
+
+$('#uploadURLForm').submit(function(e) {
+  var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+  var bar = $('.bar');
+  var progress = $('.progess')
+  var percent = $('.progress-bar');
+  var status = $('#status');
+  $(".progress").attr("style", "display: block" );
+  $('#resultArea').attr("style", "display:block");
+
+
+    e.preventDefault();
+
+    if(  ($('#url1').val() == '') ){
+      console.log('One of the input file is missing')
+      // console.log(response)
+      toastr.error('One of the input file is missing')
+
+    }else{ 
+  
+      $form = $(this)
+      var formData = new FormData(this);
+      console.log(formData);
+      formData.append('url1', $('#url1').val());
+  
+      console.log($('#url1').val());
+      
+      console.log(formData);
+
+      console.log($("#percentageS"));
+
+  
+      $.ajax({
+        xhr: function() {
+          var xhr = new window.XMLHttpRequest();
+      
+          xhr.upload.addEventListener("progress", function(evt) {
+            if (evt.lengthComputable) {
+              var percentComplete = evt.loaded / evt.total;
+              percentComplete = parseInt(percentComplete * 100);
+              var percentVal = '0%';
+              percent.width(`${percentComplete}%`);
+              percent.text(`${percentComplete}%`);
+              console.log(percentComplete);
+      
+              if (percentComplete === 100) {
+                percent.text('Upload Completed');
+      
+              }
+      
+            }
+          }, false);
+      
+          return xhr;
+        },
+          url: "https://docufix.pythonanywhere.com/upload/delimeter/url/",
+          type: "POST",
+          data: formData,
+          beforeSend: function(xhr, settings) {
+              if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                  xhr.setRequestHeader("X-CSRFToken", csrftoken);
+              }
+          },
+          success: function(response){
+              console.log('success');
+              console.log(response);
+              $(".progress").attr("style", "display: none " );
+              progress.remove()
+              $('#textareaDifferences').val(response.file1)
+              toastr.success("Content Loaded Successfully");
+              
+          },
+          error: function(response){
+              // console.log(response)
+              toastr.error(response.responseJSON);
+              console.log(response.responseJSON);
+              $(".progress").attr("style", "display: none " );
+              
+          },
+          cache: false,
+          contentType: false,
+          processData: false
+          
+      })
+  }
+})
